402bf9809e004701c7efb7e5955fc7c0ee309535
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-19 15:44:52 +0530

    Add required changes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greeting from "./greeting";
+import Home from "./home";
 const App = ()=>{
 
-    return < Greeting />
+    return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="greetings" component={<Greeting />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      );
 }
 
 export default App;
